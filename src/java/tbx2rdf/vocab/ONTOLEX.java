e3b9ed244ed8ab8eda0346f4e2c28fdb22b8edac
John McCrae <john@mccr.ae>
2014-10-08 14:21:09 +0200

    Fix namespace for OntoLex

--- src/java/tbx2rdf/vocab/ONTOLEX.java
+++ src/java/tbx2rdf/vocab/ONTOLEX.java
@@ -17,19 +17,11 @@ public class ONTOLEX {
 	public static Property entry = defaultModel.createProperty("http://www.w3.org/ns/lemon/ontolex#entry");
 	public static Property sense = defaultModel.createProperty("http://www.w3.org/ns/lemon/ontolex#sense");
 	public static Property canonicalForm = defaultModel.createProperty("http://www.w3.org/ns/lemon/ontolex#canonicalForm");
-	public static Property constituent = defaultModel.createProperty("http://www.w3.org/ns/decomp#constituent");
+	public static Property constituent = defaultModel.createProperty("http://www.w3.org/ns/lemon/decomp#constituent");
 	public static Property otherForm = defaultModel.createProperty("http://www.w3.org/ns/lemon/ontolex#otherForm");
-	public static Property identifies = defaultModel.createProperty("http://www.w3.org/ns/decomp#identifies");
-	public static Property definition = defaultModel.createProperty("http://www.w3.org/ns/lemon/ontolex#definition");
+	public static Property identifies = defaultModel.createProperty("http://www.w3.org/ns/lemon/decomp#identifies");
 	public static Property reference = defaultModel.createProperty("http://www.w3.org/ns/lemon/ontolex#reference");
-	 
-	
 	
 	public static Resource Lexicon = defaultModel.createProperty("http://www.w3.org/ns/lemon/ontolex#Lexicon");
 	public static Resource LexicalEntry = defaultModel.createProperty("http://www.w3.org/ns/lemon/ontolex#LexicalEntry");
-    
-     
-     
-       
-     
 }
