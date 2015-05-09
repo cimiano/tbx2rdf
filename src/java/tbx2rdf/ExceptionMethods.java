60a3528cdde51a9f2394e6600ef681a905575815
Víctor Rodríguez Doncel <vrodriguez@fi.upm.es>
2015-05-09 05:26:58 +0200

    Error with subjectfield has been corrected

--- src/java/tbx2rdf/ExceptionMethods.java
+++ src/java/tbx2rdf/ExceptionMethods.java
@@ -11,6 +11,12 @@ public class ExceptionMethods {
     
     public String subjectField(String param)
     {
+        if (SubjectFields.mapauris.isEmpty())
+        {
+            SubjectFields sf = new SubjectFields();
+            sf.readInternalFile();
+        }
+        
         SubjectField res=(SubjectField)SubjectFields.mapauris.get(param);
         if (res==null)
             return "unknown";
